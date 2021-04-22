d38253488dad6f59f63150759c58eedd4caf153e
jeremie <j.bels@yahoo.fr>
2021-04-22 09:42:58 +0000

    creation variables+modif main.tf

--- main.tf
+++ main.tf
@@ -8,3 +8,23 @@ terraform {
 }
 
 provider "scaleway" {}
+
+resource "scaleway_instance_volume" "volume" {
+  type       = "b_ssd"
+  size_in_gb = var.data_disk_size
+}
+
+resource "scaleway_instance_ip" "public_ip" {}
+
+resource "scaleway_instance_server" "instance" {
+  count                 = var.num_instance
+  type                  = ${count.index} == 4 ? "DEV1-XL": var.type_server
+  image                 = "ubuntu_focal"
+  name                  = "jeremie-${count.index}"
+  ip_id                 = scaleway_instance_ip.public_ip.id
+  additional_volume_ids = [scaleway_instance_volume.data.id]
+}
+
+
+
+
