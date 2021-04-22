cfc0b28f974274f651a2394fbc9957384a303d12
jeremie <j.bels@yahoo.fr>
2021-04-22 12:50:54 +0000

    travaux avant ip private

--- main.tf
+++ main.tf
@@ -7,14 +7,17 @@ terraform {
   }
 }
 
+#config provider scaleway
 provider "scaleway" {}
 
+# config disk dur
 resource "scaleway_instance_volume" "volume" {
   count      = var.num_instance
   type       = "b_ssd"
   size_in_gb = var.data_disk_size
 }
 
+# config ip public
 resource "scaleway_instance_ip" "public_ip" {
   count = var.num_instance
 }
@@ -23,17 +26,19 @@ resource "scaleway_instance_ip" "public_ip" {
 #    name = "subnet_jb"
 #}
 
+# config des instance 3 en dev1s et 1 en XL
 resource "scaleway_instance_server" "instance" {
   count                 = var.num_instance
-  type                  = "${count.index}" == 4 ? "DEV1-XL": var.type_server
+  type                  = "${count.index}" == 3 ? "DEV1-XL": var.type_server
   image                 = "ubuntu_focal"
   name                  = "jeremie-${count.index}"
   ip_id                 = scaleway_instance_ip.public_ip[count.index].id
   additional_volume_ids = [scaleway_instance_volume.volume[count.index].id]
-  security_group_id     = "${count.index}" == 4 ?  module.sg-ap.sg_id : module.sg-es.sg_id 
+  security_group_id     = "${count.index}" == 3 ?  module.sg-ap.sg_id : module.sg-es.sg_id 
 }
 
 
+# import des module pour els security group (config des ports)
 module "sg-es" {
   source = "./modules/elasticsearch"
 }
@@ -46,5 +51,4 @@ module "sg-ap" {
 #    server_id          = "fr-par-1/11111111-1111-1111-1111-111111111111"
 #    private_network_id = "fr-par-1/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"
 #}
-
 
